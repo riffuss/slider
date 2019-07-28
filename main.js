5872a615fbc806cab1fb9503747102c9b4f320e6
riffuss <riffus@mail.ru>
2019-07-28 22:54:06 +0300

    вариант с бескон. thumb-slider при клике на thumb-img переход

--- main.js
+++ main.js
@@ -15,8 +15,12 @@ document.addEventListener('DOMContentLoaded', function(){
                         'img/slide6.jpg'
                     ],
         items = [],
-        slideW = slides[0].clientWidth,
+        slideW = slider.clientWidth,
         thumbW = slideW / 3,
+        startPos = 0,
+        rightRemoved = 0,
+        leftRemoved = 0,
+        thumbArray,
         startX = 0,
         isDown = false,
         isUp = false,
@@ -31,60 +35,94 @@ document.addEventListener('DOMContentLoaded', function(){
         slides.forEach(elem => {
             items.push(elem);
         });
-        console.log(slider.clientWidth)
+        slider_track.style.width = `${slideW * slides.length}px`;
+        slides.forEach(item => item.style.width = `${slideW}px`);
         thumbSlider.style.width = `${slider.clientWidth}px`;
         thumbTrack.style.width = `${thumbW * (thumbImg.length )}px`
-        thumbImg.forEach((el, i) => {
+        thumbTrack.prepend(thumbTrack.lastElementChild);
+        thumbImg.forEach((el) => {
             el.style.width = `${thumbW}px`;
         });
-        console.log(thumbTrack.clientWidth)
     }
     slider_track.addEventListener('transitionend', function(){
         if(direct === -1){
             slider_track.appendChild(slider_track.firstElementChild);
+            thumbTrack.appendChild(thumbTrack.firstElementChild);
         }else if(direct === 1){
             slider_track.prepend(slider_track.lastElementChild);
+            thumbTrack.prepend(thumbTrack.lastElementChild);
         }
         slider_track.style.transition = 'none';
         slider_track.style.transform = `translateX(0)`;
-        setTimeout(()=>{
-            slider_track.style.transition = 'transform 0.3s ease-out';
-        }, 0)
-    });
-    thumbTrack.addEventListener('transitionend', function(){
-        if(direct === -1){
-            thumbTrack.appendChild(thumbTrack.firstElementChild);
-            thumbTrack.style.transition = 'none';
-            thumbTrack.style.transform = `translateX(0)`;
-        }
+        thumbTrack.style.transition = 'none';
+        thumbTrack.style.transform = `translateX(0)`;
     });
     function left(){
+        index >= thumbTrack.children.length - 1 ? index = 0 : index++;
         if(direct === 1){
-            slider_track.prepend(slider_track.lastElementChild);
+            let arrSlider=[];
+            for(let i = 0; i < slider_track.children.length-1 ; i++){
+                arrSlider.push(slider_track.children[i]);
+            }
+
+            for(let i = 0; i < slider_track.children.length; i++){
+                slider_track.removeChild(slider_track.children[0]);
+            }
+            slider.style.justifyContent = 'flex-start';
+            slider_track.style.justifyContent = 'flex-start';
+            slider_track.append(...arrSlider);
+
+            let arrThumb=[];
+            for(let i = 0; i < thumbTrack.children.length - 3 ; i++){
+                arrThumb.push(thumbTrack.children[i]);
+            }
+
+            for(let i = 0; i <= thumbTrack.length - 3 ; i++){
+                thumbTrack.removeChild(thumbTrack.children[0]);
+            }
+            thumbSlider.style.justifyContent = 'flex-start';
+            thumbTrack.append(...arrThumb);
         }
         direct = -1;
-        slider.style.justifyContent = 'flex-start';
+        slider_track.style.transition = 'transform 0.3s ease-out';
         slider_track.style.transform = `translateX(-${slideW}px)`;
-        index++;
         thumbTrack.style.transition = 'transform 0.3s ease-out';
         thumbTrack.style.transform = `translateX(-${thumbW}px)`;
     }
     function right(){
+        index <= 0 ? index = thumbTrack.children.length - 1 : index--;
         if(direct === -1){
-            slider_track.appendChild(slider_track.firstElementChild);
+            let arrSlider=[];
+            for(let i = slider_track.children.length-1; i > 0 ; i--){
+                arrSlider.unshift(slider_track.children[slider_track.children.length - 1]);
+                slider_track.removeChild(slider_track.children[slider_track.children.length - 1]);
+            }
+            slider.style.justifyContent = 'flex-end';
+            slider_track.style.justifyContent = 'flex-end';
+            slider_track.prepend(...arrSlider);
+
+            let arrThumb=[];
+            for(let i = thumbTrack.children.length; i > 3 ; i--){
+                arrThumb.unshift(thumbTrack.children[thumbTrack.children.length - 1]);
+                thumbTrack.removeChild(thumbTrack.children[thumbTrack.children.length - 1]);
+            }
+            thumbSlider.style.justifyContent = 'flex-end';
+            thumbTrack.prepend(...arrThumb);
         }
         direct = 1;
-        slider.style.justifyContent = 'flex-end';
-        index--;
+        slider_track.style.transition = 'transform 0.3s ease-out';
         slider_track.style.transform = `translateX(${slideW}px)`;
-        thumbTrack.prepend(thumbTrack.lastElementChild);
-        thumbTrack.style.transition = 'none';
-        thumbTrack.style.transform = `translateX(-${slideW/3}px)`;
-        setTimeout(()=>{
-            thumbTrack.style.transition = 'transform 0.3s ease-out';
-            thumbTrack.style.transform = `translateX(0)`;
-        });
+
+
+        thumbTrack.style.transition = 'transform 0.3s ease-out';
+        thumbTrack.style.transform = `translateX(${thumbW}px)`;
     }
+    thumbTrack.addEventListener('click', (e) => {
+        if(index === 0 && +e.target.dataset.id === thumbTrack.children.length - 1) next.click();
+        if(+e.target.dataset.id !== index){
+            +e.target.dataset.id >= index || (+e.target.dataset.id === 0 && index === thumbTrack.children.length - 1) ? prev.click() : next.click();
+        }
+    });
     prev.addEventListener('click', left);
     next.addEventListener('click', right);
     // Touch events
